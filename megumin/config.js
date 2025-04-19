f0b05ba2d95a4f0a76c4c8bd239f02de83074940
shiritoJ <shiritoj@gmail.com>
2025-04-19 02:11:57 -0300

    Update config.js

--- megumin/config.js
+++ megumin/config.js
@@ -10,7 +10,7 @@ import '../plugins/main-allfake.js';
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botnumber = '' //Ejemplo: +573138954963
+global.botnumber = '+5492664836487' //Ejemplo: +573138954963
 global.confirmCode = ''
 
 //• ↳𝑺𝑶𝑳𝑶 𝑫𝑬𝑺𝑨𝑹𝑹𝑶𝑳𝑳𝑨𝑫𝑶𝑹𝑬𝑺 𝑨𝑷𝑹𝑶𝑩𝑨𝑫𝑶𝑺
